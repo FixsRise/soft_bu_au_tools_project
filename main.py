b6b0a69bbe6a4b3a9a6e46846e55bcf881df46ae
Fixs <fixs228@gmail.com>
2024-05-23 18:58:57 +0200

    Squashed commit of the following:
    commit beba32a4b661def341b45f56c4452c60fe68e8d5
    Author: Fixs <fixs228@gmail.com>
    Date:   Thu May 23 18:52:13 2024 +0200
    
        third point of the project
    
    commit 8c5b3ee8ddbb190f040f919ff6e9e3068e37590c
    Author: Fixs <fixs228@gmail.com>
    Date:   Thu May 23 18:51:53 2024 +0200
    
        start point of the project

--- main.py
+++ main.py
@@ -3,4 +3,12 @@
 for i in range(5):
     print("Hellow GIT")
 
+
 print("Done")
+
+print("Data for second bracn")
+print("Data for second bracn")
+print("Data for second bracn")
+
+print("Data for third branch")
+print("Data for third branch")
