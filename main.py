8c5b3ee8ddbb190f040f919ff6e9e3068e37590c
Fixs <fixs228@gmail.com>
2024-05-23 18:51:53 +0200

    start point of the project

--- main.py
+++ main.py
@@ -3,4 +3,11 @@
 for i in range(5):
     print("Hellow GIT")
 
+
 print("Done")
+
+print("Data for second bracn")
+print("Data for second bracn")
+print("Data for second bracn")
+
+print("Data for third branch")
